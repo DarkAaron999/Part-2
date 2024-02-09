02d2939f8e0d16079fd85d046f1e9926bf93cc0f
rennicka <rennicka@sheridancollege.ca>
2024-02-08 21:15:33 -0500

    Week4 Task2 completed: Made it where the plane follows the line, and where the plane lands when holding the space bar

--- Assets/Week 4/Scripts/Plane.cs
+++ Assets/Week 4/Scripts/Plane.cs
@@ -9,12 +9,59 @@ public class Plane : MonoBehaviour
     public float newPositionThreshold = 0.2f;
     Vector2 lastPosition;
     LineRenderer lineRenderer;
+    Vector2 currentPosition;
+    Rigidbody2D rigidbody;
+    public float speed = 1;
+    public AnimationCurve landing;
+    public float landingTimer;
 
     private void Start()
     {
         lineRenderer = GetComponent<LineRenderer>();
         lineRenderer.positionCount = 1;
         lineRenderer.SetPosition(0, transform.position);
+
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+    private void FixedUpdate()
+    {
+        currentPosition = transform.position;
+        if(points.Count > 0 )
+        {
+            Vector2 direction = points[0] - currentPosition;
+            float angle = Mathf.Atan2( direction.x, direction.y ) * Mathf.Rad2Deg;
+            rigidbody.rotation = -angle;
+        }
+        rigidbody.MovePosition(rigidbody.position + (Vector2)transform.up * speed * Time.deltaTime);
+    }
+
+    private void Update()
+    {
+        if(Input.GetKey(KeyCode.Space))
+        {
+            landingTimer += 0.5f * Time.deltaTime;
+            float interpolation = landing.Evaluate(landingTimer);
+            if (transform.localScale.z < 0.1f)
+            {
+                Destroy(gameObject);
+            }
+            transform.localScale = Vector3.Lerp(Vector3.one, Vector3.zero, interpolation);
+        }
+
+        lineRenderer.SetPosition(0, transform.position);
+        if ( points.Count > 0 )
+        {
+            if(Vector2.Distance(currentPosition, points[0]) < newPositionThreshold)
+            {
+                points.RemoveAt(0);
+
+                for(int i = 0; i < lineRenderer.positionCount - 2; i++)
+                {
+                    lineRenderer.SetPosition(i, lineRenderer.GetPosition(i + 1));
+                }
+                lineRenderer.positionCount--;
+            }
+        }
     }
 
     private void OnMouseDown()
@@ -27,7 +74,7 @@ public class Plane : MonoBehaviour
     private void OnMouseDrag()
     {
         Vector2 newPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        if(Vector2.Distance(lastPosition, newPosition) > newPositionThreshold )
+        if(Vector2.Distance(lastPosition, newPosition) > newPositionThreshold)
         {
             points.Add(newPosition);
             lineRenderer.positionCount++;

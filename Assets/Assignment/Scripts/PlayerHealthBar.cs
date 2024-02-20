d3e2a1f2e6e48df3df92205a12c159b0f50db969
rennicka <rennicka@sheridancollege.ca>
2024-02-20 09:26:06 -0500

    Assignment 2 Task 5 HealthUI Completed: Added a health bar sprite, and functionality

---
+++ Assets/Assignment/Scripts/PlayerHealthBar.cs
@@ -0,0 +1,14 @@
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealthBar : MonoBehaviour
+{
+    public Slider slider;
+    public void PlayerTakeDamage(float damage)
+    {
+        slider.value -= damage;
+    }
+}

a87ea7cae3167b884001a269665d20a9170737ed
rennicka <rennicka@sheridancollege.ca>
2024-03-18 12:04:19 -0400

    Task-fixing

--- Assets/Week 7/Scripts/GoalkeeperController.cs
+++ Assets/Week 7/Scripts/GoalkeeperController.cs
@@ -6,6 +6,7 @@ public class GoalkeeperController : MonoBehaviour
 {
     public Rigidbody2D rb;
     public GameObject goalkeeper;
+    public float distanceOff = 5;
 
     // Start is called before the first frame update
     void Start()
@@ -16,7 +17,16 @@ public class GoalkeeperController : MonoBehaviour
     private void FixedUpdate()
     {
         rb.transform.position = transform.position;
- 
+
+        if (Controller.SelectedPlayer == null) return;
+
+        Vector3 player = Controller.SelectedPlayer.transform.position;
+
+        Vector3 dirtion = (Controller.SelectedPlayer.transform.position - transform.position).normalized;
+
+        // rb.position = transform.position + dirtion*distanceOff;
+        goalkeeper.transform.position = transform.position + dirtion * distanceOff;
+
     }
 
     // Update is called once per frame
